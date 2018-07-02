dcead7dddd39a33f388e9a679706beb71df19c6b
Tim Urista <timothy.urista@gmail.com>
2018-07-01 20:58:26 -0700

    add validation to user _slug params and remove old _id param #4

--- pages/users/_id.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Single User view</h1>
-    <h2>ID: {{ $route.params.id }}</h2>
-  </div>
-</template>
---
+++ pages/users/_slug.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Single User view by slug</h1>
+    <h2>SLUG: {{ $route.params.slug }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  validate(data) {
+    return /^[-a-z0-9]+$/.test(data.params.slug);
+  }
+}
+</script>
+
