e0701778c4bd5481f000abc9aa1743d8baaa03d7
Tim Urista <timothy.urista@gmail.com>
2018-07-02 05:55:23 -0700

    add nested sub route to users.vue file using nuxt-child

---
+++ pages/users.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Slug</h2>
+    <input type="text" v-model="userSlug" >
+    <button @click="onLoadUser">Load User</button>
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userSlug: ''
+    }
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/'+this.userSlug)
+    }
+  }
+}
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,22 +1,3 @@
 <template>
-  <div>
-    <h2>Slug</h2>
-    <input type="text" v-model="userSlug" >
-    <button @click="onLoadUser">Load User</button>
-  </div>
+  <p>Please enter an ID and click the button</p>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      userSlug: ''
-    }
-  },
-  methods: {
-    onLoadUser() {
-      this.$router.push('/users/'+this.userSlug)
-    }
-  }
-}
-</script>
