a570c2b19b2cb1e07acdc74522ac633a0211bf34
Tim Urista <timothy.urista@gmail.com>
2018-07-03 06:32:56 -0700

    add users layout #8

---
+++ layouts/users.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header></header>
+    <nav>USERS SECTION</nav>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+nav {
+  background-color: #0b0b0b;
+  color: #fff;
+  font-size: 32px;
+}
+</style>
