9db2f73e3ed4f5b9f50ee44581e8b9780c79c054
Tim Urista <timothy.urista@gmail.com>
2018-07-17 20:31:11 -0700

    add payload to context to limit http gets

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,7 +89,10 @@ module.exports = {
         .then( res => {
           const routes = []
           for (const key in res.data) {
-            routes.push('/posts/' + key)
+            routes.push({
+              route: '/posts/' +
+              key, payload: { postData: res.data[key]}
+            })
           }
           return routes;
         })
